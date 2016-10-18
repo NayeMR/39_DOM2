852a2ba19386292053b972ad7abb5af92ab07153
NayeMR <naye.mirandys@gmail.com>
2016-10-18 10:56:28 -0900

    Agregue carpeta inicial del DOM2

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function validateForm(){
+	/* Escribe tú código aquí */
+	var opcion1  = document.querySelectorAll("option");
+document.getElementsByTagName("opcion1") = hola ;
+console.log("hola" + hola);
+}
